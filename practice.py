5d22df05fe61abcee2c66631a1947709dae8704a
Guhan-AR <168805253+Guhan-AR@users.noreply.github.com>
2024-11-09 16:17:43 +0530

    finished merge sort

---
+++ practice.py
@@ -0,0 +1,26 @@
+def merge_sort(arr):
+    if len(arr)>1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+        i=j=k=0
+        while i<len(left_arr) and j<len(right_arr):
+            if left_arr[i]<right_arr[j]:
+                arr[k]=left_arr[i]
+                i+=1
+            else:
+                arr[k]=right_arr[j]
+                j+=1
+            k+=1
+        while i<len(left_arr):
+            arr[k]=left_arr[i]
+            i+=1
+            k+=1
+        while j<len(right_arr):
+            arr[k]=right_arr[j]
+            j+=1
+            k+=1
+a=[0,2,5,1,6]
+merge_sort(a)
+print(a)
--- sorting.py
+++ sorting.py
@@ -21,9 +21,34 @@ class sorting():
                 self.array[j+1]=self.array[j]
                 j-=1
             self.array[j+1]=key
+    def merge_sort(self,array=None):
+        if array is None:
+            array=self.array
+        if len(array)>1:
+            left_arr=array[:len(array)//2]
+            right_arr=array[len(array)//2:]
+            self.merge_sort(left_arr)
+            self.merge_sort(right_arr)
+            i=j=k=0
+            while i<len(left_arr) and j<len(right_arr):
+                if left_arr[i]<right_arr[j]:
+                    array[k]=left_arr[i]
+                    i+=1
+                else:
+                    array[k]=right_arr[j]
+                    j+=1
+                k+=1
+            while i<len(left_arr):
+                array[k]=left_arr[i]
+                i+=1
+                k+=1
+            while j<len(right_arr):
+                array[k]=right_arr[j]
+                j+=1
+                k+=1
 
 a=[3,2,0,0,4,6]
 sorter=sorting(a)
 print("Before sorting:",a)
-sorter.insertion_sort()
+sorter.merge_sort()
 print("After sorting:",a)
