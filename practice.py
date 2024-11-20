c1edb0410f22573b19f5ee3d69c81ce24b1c0e49
Guhan-AR <168805253+Guhan-AR@users.noreply.github.com>
2024-11-20 11:57:19 +0530

    leetcode problem jump game

--- practice.py
+++ practice.py
@@ -333,3 +333,30 @@ def change_alternate_element(array):
     return a
 # a=[1,2,3,4,5,6,7]
 # print(change_alternate_element(a))
+
+def add_higher_index(array):
+    summer=0
+    maxi=0
+    for i in array:
+        for j in str(i):
+            maxi=max(maxi,int(j))
+        summer+=maxi
+        maxi=0
+    return summer
+# a=[7,10,32]
+# print(add_higher_index(a))
+
+class Solution:
+    def jump(self, nums: list[int]) -> int:
+        near = far = jumps = 0
+        while far < len(nums) - 1:
+            farthest = 0
+            for i in range(near, far + 1):
+                farthest = max(farthest, i + nums[i])
+            near = far + 1
+            far = farthest
+            jumps += 1
+        return jumps
+# s=Solution()
+# print(s.jump([2,3,1,1,4]))
+
