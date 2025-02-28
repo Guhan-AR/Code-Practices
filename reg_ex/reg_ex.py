aadaacba84af413de7c4f8d21e971de923bf135a
Guhan-AR <arguhan55@gmail.com>
2025-02-28 14:53:31 +0530

    completed regex

--- reg_ex/reg_ex.py
+++ reg_ex/reg_ex.py
@@ -1,12 +1,14 @@
 import re
 
 string = "123b456abcg1234ababcccvfg35hu45hudbfgh23789"
-# pattern_obj = re.compile(r"abc")
-pattern_obj = re.compile(r"[a-d]")
+pattern_obj = re.compile(r"abc")
+# pattern_obj = re.compile(r"[a-d]")
 
-match_finder = pattern_obj.finditer(string)
+# match_finder = pattern_obj.finditer(string)
 # match_finder = pattern_obj.search(string)
+# match_finder = pattern_obj.split(string)
+match_finder = pattern_obj.sub(' hi ',string)
 
-for matching in match_finder:
-    print(matching)
-# print(match_finder)
+# for matching in match_finder:
+#     print(matching)
+print(match_finder)
